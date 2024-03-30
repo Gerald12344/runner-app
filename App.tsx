2deea8dfb97c7923311ae03b4f9115d3c0d8132d
Harvey Randall <harvey.randall@aol.com>
2024-03-30 23:11:05 +0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import "./global.css";
+import { Text, View } from "react-native";
+import MainComp from "./app/components/main";
+
+export default function App() {
+  return (
+    <View>
+      <MainComp />
+    </View>
+  );
+}
---
+++ app/components/main.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function MainComp() {
+  return (
+    <View className="mt-20 w-full">
+      <Text className="text-5xl bg-blue-500">Test!!</Text>
+      <Text className="text-5xl bg-red-500">Test!!</Text>
+      <Text className="text-5xl bg-yellow-500">Test!!</Text>
+      <Text className="text-5xl bg-green-500">Test!!</Text>
+    </View>
+  );
+}
